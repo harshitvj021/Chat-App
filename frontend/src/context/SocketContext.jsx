5e19c1c5e87cf8b46e6538e6b048e663c67b397f
Harshit Singh <harshitvj021@gmail.com>
2025-01-19 23:26:51 +0530

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://localhost:5000", {
+			const socket = io("https://chat-app-x6r4.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
